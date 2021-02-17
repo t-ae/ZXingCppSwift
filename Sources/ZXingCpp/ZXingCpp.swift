ab6742103ee0123229aa406cefe65f5efdf1c124
kenta okamura <okamura@qoncept.co.jp>
2021-02-17 12:52:26 +0900

    remove Mat struct

--- Sources/ZXingCpp/ZXingCpp.swift
+++ Sources/ZXingCpp/ZXingCpp.swift
@@ -10,22 +10,8 @@ public enum ImageFormat: Int32 {
     case XBGR = 6
 }
 
-public struct Mat {
-    public var data: UnsafePointer<UInt8>
-    public var rows: Int32
-    public var cols: Int32
-    public var format: ImageFormat
-    
-    public init(data: UnsafePointer<UInt8>, rows: Int32, cols: Int32, format: ImageFormat) {
-        self.data = data
-        self.rows = rows
-        self.cols = cols
-        self.format = format
-    }
-}
-
-public func QRDecode(mat: Mat) -> String? {
-    if let result = qr_decode(mat.data, mat.cols, mat.rows, mat.format.rawValue) {
+public func QRDecode(data: UnsafePointer<UInt8>, rows: Int32, cols: Int32, format: ImageFormat) -> String? {
+    if let result = qr_decode(data, cols, rows, format.rawValue) {
         return CppString(result).toString()
     }
     return nil
